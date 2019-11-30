35d7e8d4b9489ef68f9ab8d364b51a7e5e25f428
judexie <jude.xie@yahoo.com>
2019-11-30 13:55:22 +0800

    yes

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ id=$(ps -ef|grep sample.jar|grep -v grep|awk '{print $2}')
 if [ ! $id ]; then echo "process not started"; else kill -9 $id;fi
 sleep 2s;
 nohup java -jar /opt/ansible/sample.jar  > /tmp/nohup.out 2>&1 &
+echo yes
